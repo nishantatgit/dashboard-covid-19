89157a08f1b39b5203608e53075b444257ffb766
nku107 <nishant.kumar.shreevastav@gmail.com>
2020-04-14 17:34:13 +0530

    removed unwanted files

--- client/utilities/setIntersectionObserver.js
+++ client/utilities/setIntersectionObserver.js
@@ -1,25 +1,30 @@
-const setIntersectionObserver = ( options  = {} , target, onTargetEntry, onTargetLeave  ) => {
-  const {
-    rootMargin = '0px',
-    threshold = 0,
-    root = null
-  } = options;
+const setIntersectionObserver = (
+  options = {},
+  target,
+  onTargetEntry,
+  onTargetLeave
+) => {
+  const { rootMargin = '0px', threshold = 0, root = null } = options;
 
   const wrappedCallback = (entries) => {
     let { isIntersecting } = entries[0];
-    if(isIntersecting && onTargetEntry){
+    if (isIntersecting && onTargetEntry) {
       onTargetEntry();
     }
-    if(!isIntersecting && onTargetLeave){
+    if (!isIntersecting && onTargetLeave) {
       onTargetLeave();
     }
-  }
+  };
 
   const scrollArea = document.getElementById(root) || null;
-  const observer = new IntersectionObserver(wrappedCallback, { root : scrollArea, rootMargin, threshold});
+  const observer = new IntersectionObserver(wrappedCallback, {
+    root: scrollArea,
+    rootMargin,
+    threshold,
+  });
   const targetEl = document.getElementById(target);
 
   observer.observe(targetEl);
-}
+};
 
 export default setIntersectionObserver;

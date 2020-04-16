9301ffbd163279d890e417609313c8813053c021
nku107 <nishant.kumar.shreevastav@gmail.com>
2020-04-16 16:52:39 +0530

    chropleth map refactor

--- client/components/molecules/Choropleth/Choropleth.js
+++ client/components/molecules/Choropleth/Choropleth.js
@@ -1,27 +1,57 @@
 import React from 'react';
-import D3Choropleth from './D3Choropleth';
+import * as d3 from 'd3';
 import './Choropleth.scss';
-class Choropleth extends React.Component {
-  constructor(props) {
-    super(props);
-    this.mapRef = React.createRef();
+import { SVG, COLORS } from '../../../constants';
+import { useEffect } from 'react';
+
+function Choropleth(props) {
+  const { data } = props;
+  const { geoData, geoJSON } = data;
+  const { features } = geoJSON;
+  const { height: mapHeight, width: mapWidth, viewBox } = SVG;
+  const updatedFeatures = injectDataToFeatures(features, geoData);
+
+  const color = d3
+    .scaleQuantize()
+    .range(COLORS.choropleth)
+    .domain([0, geoData.data[0][geoData.key]]);
+  const projection = d3.geoMercator().fitSize([mapWidth, mapHeight], geoJSON);
+  const pathFunc = d3.geoPath().projection(projection);
+  return (
+    <section className="choropleth-map-container">
+      <svg viewBox={viewBox} stroke={COLORS.white} strokeWidth={0.5}>
+        {drawMap()}
+      </svg>
+    </section>
+  );
+
+  function drawMap() {
+    const { key, geoKey } = geoData;
+    return updatedFeatures.map((feature) => (
+      <path
+        d={pathFunc(feature)}
+        fill={color(feature.properties[key])}
+        key={feature.properties[geoKey]}
+      ></path>
+    ));
   }
 
-  componentDidMount() {
-    const {
-      props: { data },
-    } = this;
-    console.log('choropleth data --> ', data);
-    D3Choropleth.create(this.mapRef, data);
-  }
+  function injectDataToFeatures(features, geoData) {
+    const { geoKey: key, data } = geoData;
+    features.forEach((feature) => {
+      const correspondingData = data.filter(
+        (item) => item[key] === feature.properties[key]
+      )[0];
 
-  render() {
-    const {
-      props: { className },
-    } = this;
-    return (
-      <section className="choropleth-map-container" ref={this.mapRef}></section>
-    );
+      if (correspondingData) {
+        const dataKeys = Object.keys(correspondingData);
+
+        dataKeys.forEach(
+          (key) => (feature.properties[key] = correspondingData[key])
+        );
+      }
+    });
+    return features;
   }
 }
 

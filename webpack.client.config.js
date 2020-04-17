3d1b712a64b9dac74bd8346bb2d2419f13657fe2
nku107 <nishant.kumar.shreevastav@gmail.com>
2020-04-17 16:00:38 +0530

    chaged floder structure

--- webpack.client.config.js
+++ webpack.client.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   entry: {
-    client: './client/index.js',
+    client: './client/pages/HomePage/index.js',
+    america: './client/pages/HomePageUSA/index.js',
   },
   output: {
     path: __dirname + '/public',
